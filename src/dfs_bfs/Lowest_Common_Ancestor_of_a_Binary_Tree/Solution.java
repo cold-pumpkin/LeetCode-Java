543792d258bd3f03735c0b657bb8f771944c030b
adorno <pch4623@gmail.com>
2020-11-25 22:26:03 +0900

    236. Lowest Common Ancestor of a Binary Tree
    left, right 로 DFS 탐색해 가다가, 둘 다 null이 아니라면 부모노드가 LCA.
    둘 중 하나가 null이라면, null이 아닌 족이 LCA가 됨.
    ex) p를 left에서 발견 & right가 null이라면, q는 p와 같은 쪽 branch에
    있다는 의미.

--- src/dfs_bfs/Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.java
+++ src/dfs_bfs/Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.java
@@ -1,2 +1,18 @@
-package dfs_bfs.Lowest_Common_Ancestor_of_a_Binary_Tree;public class Solution {
+package dfs_bfs.Lowest_Common_Ancestor_of_a_Binary_Tree;
+
+import stack_queue.TreeNode;
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+        else
+            return left != null ? left : right;
+    }
 }
