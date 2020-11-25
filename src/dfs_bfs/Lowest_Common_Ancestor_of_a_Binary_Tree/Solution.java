71e97385180408d3d812079b9d1bafb553a96f49
adorno <pch4623@gmail.com>
2020-11-25 22:23:28 +0900

    TreeNode 구현 코드 import

---
+++ src/dfs_bfs/Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.java
@@ -0,0 +1,2 @@
+package dfs_bfs.Lowest_Common_Ancestor_of_a_Binary_Tree;public class Solution {
+}
--- src/dfs_bfs/Path_Sum_III/Solution.java
+++ src/dfs_bfs/Path_Sum_III/Solution.java
@@ -1,4 +1,27 @@
 package dfs_bfs.Path_Sum_III;
 
+import stack_queue.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> cache = new HashMap<>();  // {누적 합: 도달한 경우}
+        cache.put(0, 1);
+        return dfs(root, 0, sum, cache);
+    }
+
+    public int dfs(TreeNode node, int currSum, int target, Map<Integer, Integer> cache) {
+        if (node == null)
+            return 0;
+        currSum += node.val;
+        int res = cache.getOrDefault(currSum - target, 0);  // 지나온 루트에 target을 만족한 경우가 있는지 확인
+        cache.put(currSum, cache.getOrDefault(currSum, 0) + 1);  // 캐시 업데이트
+
+        res += dfs(node.left, currSum, target, cache) + dfs(node.right, currSum, target, cache);
+
+        cache.put(currSum, cache.get(currSum) - 1);  // 한쪽 다 돌고 다른 branch 가면 누적합 필요없어짐
+        return res;
+    }
 }
