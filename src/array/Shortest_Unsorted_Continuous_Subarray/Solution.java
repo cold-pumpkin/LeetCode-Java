400bfa817c13ac7ba09f8bc6d090f062e05819c7
adorno <pch4623@gmail.com>
2020-12-06 21:50:20 +0900

    581. Shortest Unsorted Continuous Subarray

---
+++ src/array/Shortest_Unsorted_Continuous_Subarray/Solution.java
@@ -0,0 +1,18 @@
+package array.Shortest_Unsorted_Continuous_Subarray;
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int start = -1, end = -2;
+        int min = nums[n-1], max = nums[0];
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-1-i]);
+            if (nums[i] < max)
+                end = i;
+            if (nums[n-1-i] > min)
+                start = n-1-i;
+        }
+        return end - start + 1;
+    }
+}
