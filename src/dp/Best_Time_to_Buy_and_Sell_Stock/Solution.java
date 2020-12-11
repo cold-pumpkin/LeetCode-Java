3371a144bb284a3f22900bd1e67897fe0f59f8e4
adorno <pch4623@gmail.com>
2020-12-11 22:54:10 +0900

    1. Two Sum
    Map에 타겟과 현재 값의 차이 키로, 인덱스를 값으로 저장해 두고, 타겟값을
    만족시킬 키값이 존재하는지 확인.

---
+++ src/dp/Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -0,0 +1,15 @@
+package dp.Best_Time_to_Buy_and_Sell_Stock;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice)
+                minPrice = price;
+            else if (price - minPrice > maxProfit)
+                maxProfit = price - minPrice;
+        }
+        return maxProfit;
+    }
+}
---
+++ src/hashtable/Two_Sum/Solution.java
@@ -0,0 +1,18 @@
+package hashtable.Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();  // {num: idx}
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp)) {
+                return new int[] { map.get(comp), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
