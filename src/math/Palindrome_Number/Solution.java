adbb5b1f08afcb434957b1716ee8458e931b010d
adorno <pch4623@gmail.com>
2020-12-16 21:38:23 +0900

    9. Palindrome Number
    기본 숫자 길이의 반까지 뒤집어서 비교.

---
+++ src/math/Palindrome_Number/Solution.java
@@ -0,0 +1,22 @@
+package math.Palindrome_Number;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev / 10;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(121));
+        System.out.println(new Solution().isPalindrome(1221));
+    }
+}
