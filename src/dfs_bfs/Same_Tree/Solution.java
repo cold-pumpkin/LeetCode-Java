8f447d67752dd99455a287edc07fed5142921df5
adorno <pch4623@gmail.com>
2020-12-19 11:41:43 +0900

    100. Same Tree 1) Recursion  트리를 재귀적으로 탐색하며 두 노드의 값이 같은지 확인 2) Iteration  레벨별로 노드들을 큐에 넣고 빼며 두 노드의 값이 같은지 확인

--- src/dfs_bfs/Same_Tree/Solution.java
+++ src/dfs_bfs/Same_Tree/Solution.java
@@ -1,2 +1,62 @@
-package dfs_bfs.Same_Tree;public class Solution {
+package dfs_bfs.Same_Tree;
+
+import stack_queue.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    // 1) Recursion
+    /*
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    */
+
+    // 2) Iteration
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+        Deque<TreeNode> deqP = new ArrayDeque<>();
+        Deque<TreeNode> deqQ = new ArrayDeque<>();
+        deqP.add(p);
+        deqQ.add(q);
+
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+
+            if (!check(p.left, q.left))
+                return false;
+            if(p.left != null) {
+                deqP.addLast(p.left);
+                deqQ.addLast(q.left);
+            }
+
+            if (!check(p.right, q.right))
+                return false;
+            if(p.right != null) {
+                deqP.addLast(p.right);
+                deqQ.addLast(q.right);
+            }
+        }
+        return true;
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return true;
+    }
 }
