1a835138790c1cef7b9ca7eff45ae19b41378f43
adorno <pch4623@gmail.com>
2020-12-24 16:33:33 +0900

    168. Excel Sheet Column Title
    숫자를 26으로 나눈 나머지 번째 문자가 열 이름의 마지막 문자가 됨.

---
+++ src/math/Excel_Sheet_Column_Title/Solution.java
@@ -0,0 +1,13 @@
+package math.Excel_Sheet_Column_Title;
+
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+        while (n > 0) {
+            n--;
+            result.insert(0, (char)('A' + (n % 26)));
+            n /= 26;
+        }
+        return result.toString();
+    }
+}
